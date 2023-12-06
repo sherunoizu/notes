afcf366b08a43ff9585a03e7692054b84a6cf414
Nikita Zapletin <nikitazapletin@gmail.com>
2023-12-06 19:54:58 +0300

    Edit-Note page design

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { IoIosArrowBack } from "react-icons/io";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 export const EditNote = () => {
   return (
-    <div>EditNote</div>
-  )
-}
+    <section>
+      <header className="create-note__header">
+        <Link to="/" className="btn">
+          <IoIosArrowBack />
+        </Link>
+        <button className="btn lg primary">Save</button>
+        <button className="btn lg danger">
+          <RiDeleteBin6Line />
+        </button>
+      </header>
+      <form action="" className="create-note__form">
+        <input type="text" placeholder="Title" autoFocus />
+        <textarea rows="28" placeholder="Note details..."></textarea>
+      </form>
+    </section>
+  );
+};
