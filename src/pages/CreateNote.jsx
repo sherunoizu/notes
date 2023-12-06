aa4df7c6031d37c8e15ef77afdfd8f6d1681ef19
Nikita Zapletin <nikitazapletin@gmail.com>
2023-12-06 19:51:08 +0300

    Create-note page design

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 export const CreateNote = () => {
   return (
-    <div>CreateNote</div>
-  )
-}
+    <section>
+      <header className="create-note__header">
+        <Link to="/" className="btn">
+          <IoIosArrowBack />
+        </Link>
+        <button className="btn lg primary">Save</button>
+      </header>
+      <form action="" className="create-note__form">
+        <input type="text" placeholder="Title" autoFocus />
+        <textarea rows="28" placeholder="Note details..."></textarea>
+      </form>
+    </section>
+  );
+};
